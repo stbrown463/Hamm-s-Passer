06b67cb5b22704fc6e43d38987289775142c1287
Sam Brown <stbrown463@gmail.com>
2019-01-07 10:08:39 -0600

    Made one Bar

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
 console.log('Tapper!');
+
+const canvas = document.getElementById('board');
+// console.log(canvas);
+
+const ctx = canvas.getContext('2d');
+console.log(ctx);
+
+function makeBar (x, y, width, height, color) {
+	ctx.beginPath();
+	ctx.rect(x, y, width, height);
+	ctx.fillStyle = color;
+	ctx.fill();
+}
+
+makeBar(0, 100, 600, 50, 'brown');
+
+// class Bar {
+// 	constructor () {
+
+// 	}
+// }
+
+// class Patron {
+// 	constructor() {
+
+// 	}
+// }
