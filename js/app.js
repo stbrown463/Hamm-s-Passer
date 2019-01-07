9beba04887affd9737e0ae7e1bfd39370ee9c750
Sam Brown <stbrown463@gmail.com>
2019-01-07 10:57:00 -0600

    Modular bars and taps

--- js/app.js
+++ js/app.js
@@ -15,14 +15,64 @@ function makeBar (x, y, width, height, color) {
 
 makeBar(0, 100, 600, 50, 'brown');
 
-// class Bar {
-// 	constructor () {
+class Bar {
+	constructor (x, y, width, height, color) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.color = color;
+	}
+	draw () {
+		ctx.beginPath();
+		ctx.rect(this.x, this.y, this.width, this.height);
+		ctx.fillStyle = this.color;
+		ctx.fill();
+	}
+}
 
-// 	}
-// }
+
 
 // class Patron {
 // 	constructor() {
 
 // 	}
 // }
+
+const board = {
+	numBars: 3,
+	bars: [],
+	makeBars () {
+		for (let i = 0; i < this.numBars; i++) {
+			const bar = new Bar (0, 100 + (i * (700 / this.numBars)), 600, 50, 'brown')
+			bar.draw()
+			board.bars.push(bar);
+		}
+	},
+	makeTaps() {
+		for (let i = 0; i < this.numBars; i++) {
+			const bar = new Bar (770, 100 + (i * (700 / this.numBars)), 30, 50, 'brown')
+			bar.draw()
+			board.bars.push(bar);
+		}
+	}
+}
+
+board.makeBars();
+board.makeTaps();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
